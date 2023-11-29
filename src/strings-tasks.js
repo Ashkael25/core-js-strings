9b249c2011eaebbd6cd9e818ea6dc8d428bebc30
Aleksandr <Maksimeca@rambler.ru>
2023-11-29 11:41:54 +0300

    feat: add file

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -359,8 +359,8 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence.split(' ').sort((a, b) => b.length - a.length)[0];
 }
 
 /**
@@ -373,8 +373,13 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const string = str.split(' ');
+  const res = [];
+  for (let i = 0; i < string.length; i += 1) {
+    res.push(string[i].split('').reverse().join(''));
+  }
+  return res.join(' ');
 }
 
 /**
